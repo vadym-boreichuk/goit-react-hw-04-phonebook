29b43747836137f81aad68aa7b37f2cfaa7c9c3f
Борейчук Вадим <gefeste111@gmail.com>
2023-03-15 15:54:40 +0100

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,97 +1,69 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { Filter } from '../Filter/Filter';
 import { ContactList } from '../ContactList/ContactList';
 import { ContactForm } from '../ContactForm/Form';
 import { Div, Container, Title } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onDelete = contactId => {
+    setContacts(contacts.filter(({ id }) => id !== contactId));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  onDelete = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  addContact = event => {
+  const addContact = event => {
     event.preventDefault();
     const name = event.currentTarget.elements.name.value;
     const number = event.currentTarget.elements.number.value;
     const normalizedName = name.toLowerCase();
     const contact = {
       id: nanoid(),
-      name: name,
-      number: number,
+      name,
+      number,
     };
 
     if (
-      this.state.contacts.find(
-        contact => contact.name.toLowerCase() === normalizedName
-      )
+      contacts.find(contact => contact.name.toLowerCase() === normalizedName)
     ) {
       return alert(`${name} is already in contacts!`);
     }
-    this.setState(({ contacts }) => ({
-      contacts: [...contacts, contact],
-    }));
+    setContacts(prevContacts => [contact, ...prevContacts]);
 
     event.currentTarget.reset();
   };
 
-  filterContacts = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const filterContacts = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    // console.log('render');
-    const { addContact, filterContacts, state, onDelete } = this;
-    return (
-      <Container>
-        <Div>
-          <Title>Phonebook</Title>
+  return (
+    <Container>
+      <Div>
+        <Title>Phonebook</Title>
 
-          <ContactForm addContact={addContact} />
-        </Div>
-        <Div>
-          <Title>Contacts</Title>
-          <Filter value={state.filter} onChange={filterContacts} />
-          <ContactList
-            onClick={onDelete}
-            contacts={this.getFilteredContacts()}
-          />
-        </Div>
-      </Container>
-    );
-  }
-}
+        <ContactForm addContact={addContact} />
+      </Div>
+      <Div>
+        <Title>Contacts</Title>
+        <Filter value={filter} onChange={filterContacts} />
+        <ContactList onClick={onDelete} contacts={getFilteredContacts()} />
+      </Div>
+    </Container>
+  );
+};
